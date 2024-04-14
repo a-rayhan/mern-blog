a384aa1d07c5f3ae2ac16066959fe00ce9f81dcc
a-rayhan <aburayhandh@gmail.com>
2024-04-14 19:21:10 +0600

    Add funtionality in signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import { Spinner } from "flowbite-react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success == false){
+        return setErrorMessage(data.message)
+      }
+
+      setLoading(false);
+
+      if(res.ok){
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-[80%] max-w-[400px] mx-auto">
@@ -10,13 +56,16 @@ export default function SignIn() {
           Sign in to continue
         </h1>
 
+        {errorMessage && <Alert className="mb-5 font-medium font-inter text-base" color={'failure'}>{errorMessage}</Alert>}
+
         <form>
           <input
             name="email"
-            type="text"
+            type="email"
             id="email"
             placeholder="Email"
             className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-medium font-inter mb-3"
+            onChange={handleChange}
           />
 
           <input
@@ -25,10 +74,14 @@ export default function SignIn() {
             id="password"
             placeholder="Password"
             className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-medium font-inter mb-4"
+            onChange={handleChange}
           />
 
-          <button type="submit" className="bg-[#03aaff] w-[100%] rounded-md py-2 border border-[#03aaff] focus:bg-transparent font-medium font-inter text-white text-xl">
-            Sign in
+          <button type="submit" onClick={handleSubmit} disabled={loading} className="bg-[#03aaff] w-[100%] rounded-md py-2 border border-[#03aaff] focus:bg-transparent font-medium font-inter text-white text-xl">
+          {loading ? (<>
+            <Spinner size='sm' />
+            <span className="pl-3">Loading...</span>
+            </>) : 'Sign in'}
           </button>
         </form>
 
