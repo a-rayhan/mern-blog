ab801fd7ddda6ddfc1129de221a14edf899ea97a
a-rayhan <aburayhandh@gmail.com>
2024-04-15 21:18:06 +0600

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,21 @@
-import { Spinner } from "flowbite-react";
+import { Alert, Spinner } from "flowbite-react";
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -18,12 +25,11 @@ export default function SignIn() {
     e.preventDefault();
 
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields.");
+      return dispatch(signInFailure("Please fill out all fields."));
     }
 
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -34,18 +40,16 @@ export default function SignIn() {
 
       const data = await res.json();
 
-      if(data.success == false){
-        return setErrorMessage(data.message)
+      if (data.success == false) {
+        dispatch(signInFailure(data.message));
       }
 
-      setLoading(false);
-
-      if(res.ok){
-        navigate('/');
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -56,7 +60,14 @@ export default function SignIn() {
           Sign in to continue
         </h1>
 
-        {errorMessage && <Alert className="mb-5 font-medium font-inter text-base" color={'failure'}>{errorMessage}</Alert>}
+        {errorMessage && (
+          <Alert
+            className="mb-5 font-medium font-inter text-base"
+            color={"failure"}
+          >
+            {errorMessage}
+          </Alert>
+        )}
 
         <form>
           <input
@@ -64,7 +75,7 @@ export default function SignIn() {
             type="email"
             id="email"
             placeholder="Email"
-            className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-medium font-inter mb-3"
+            className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-inter mb-3"
             onChange={handleChange}
           />
 
@@ -73,15 +84,24 @@ export default function SignIn() {
             type="password"
             id="password"
             placeholder="Password"
-            className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-medium font-inter mb-4"
+            className="w-[100%] rounded-md px-5 bg-[#f3f3f6] border border-[#f3f3f6] focus:bg-transparent font-inter mb-4"
             onChange={handleChange}
           />
 
-          <button type="submit" onClick={handleSubmit} disabled={loading} className="bg-[#03aaff] w-[100%] rounded-md py-2 border border-[#03aaff] focus:bg-transparent font-medium font-inter text-white text-xl">
-          {loading ? (<>
-            <Spinner size='sm' />
-            <span className="pl-3">Loading...</span>
-            </>) : 'Sign in'}
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            disabled={loading}
+            className="bg-[#03aaff] w-[100%] rounded-md py-2 border border-[#03aaff] font-medium font-inter text-white text-xl"
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">Loading...</span>
+              </>
+            ) : (
+              "Sign in"
+            )}
           </button>
         </form>
 
